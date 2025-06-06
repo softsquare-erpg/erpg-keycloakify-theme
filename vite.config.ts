3718f49dda26dd481e05821aac1e9f73814e32fd
l3omb-io <adsadawutbomb.23@gmail.com>
2025-06-06 19:46:44 +0700

    🔧 update theme details and configuration for erpg-keycloakify-theme

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { keycloakify } from "keycloakify/vite-plugin";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         keycloakify({
+            groupId: "com.softsquare.keycloakify",
+            artifactId: "erpg-keycloakify-theme",
+            themeVersion: "1.0.0",
+            themeName: "erpg-keycloakify-theme",
             accountThemeImplementation: "none"
         })
     ]
