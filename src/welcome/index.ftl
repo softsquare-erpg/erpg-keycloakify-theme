28f9dea6148ad6f7c4e4b8b84da2b1cadb2d2d46
l3omb-io <adsadawutbomb.23@gmail.com>
2025-06-22 22:18:39 +0700

    ✨ welcome page and theme properties with keycloak configuration

---
+++ src/welcome/index.ftl
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="robots" content="noindex, nofollow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Navigating...</title>
+        <script type="text/javascript">
+            console.log(`Navigating to ${properties.client_1_home_url}`);
+            window.location.href = `${properties.client_1_home_url}`;
+        </script>
+    </head>
+    <body data-page-id="welcome"></body>
+</html>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import react from "@vitejs/plugin-react";
+import { cp } from "fs/promises";
 import { keycloakify } from "keycloakify/vite-plugin";
+import { join, resolve } from "path";
 import { defineConfig } from "vite";
 import pkg from "./package.json" assert { type: "json" };
 
@@ -22,6 +24,27 @@ export default defineConfig({
             keycloakVersionTargets: {
                 "22-to-25": false,
                 "all-other-versions": `${artifactId}.jar`
+            },
+            startKeycloakOptions: {
+                dockerExtraArgs: [`-e KC_CLIENT_1_URL=https://my-theme.keycloakify.dev`],
+                keycloakExtraArgs: [`--spi-theme-welcome-theme=${themeName}`]
+            },
+            postBuild: async ({ keycloakifyBuildDirPath }) => {
+                const destThemePath = join(
+                    keycloakifyBuildDirPath,
+                    "resources/theme",
+                    themeName,
+                    "welcome"
+                );
+                const sourceWelcomePath = resolve(__dirname, "src", "welcome");
+
+                try {
+                    await cp(sourceWelcomePath, destThemePath, {
+                        recursive: true
+                    });
+                } catch (error) {
+                    throw new Error(`Error copying welcome folder: ${error}`);
+                }
             }
         })
     ]
